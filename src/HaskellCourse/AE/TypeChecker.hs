337bbd79ffc5b961c92c971f08373567bd90d0cb
joshcough <joshcough@gmail.com>
2015-01-02 20:41:03 -0500

    fixes

--- src/HaskellCourse/AE/TypeChecker.hs
+++ src/HaskellCourse/AE/TypeChecker.hs
@@ -2,33 +2,31 @@ module HaskellCourse.AE.TypeChecker where
 
 import HaskellCourse.AE.AST
 
-data Type = NumT | BoolT | ArrowT Type Type | TyVar deriving (Eq, Show)
+data Type = NumT | BoolT | ArrowT Type Type | Poly deriving (Eq, Show)
 
-tc :: Exp -> Type
-tc (LitInt  i)  = NumT
-tc (LitBool b)  = BoolT
-tc (PrimExp p)  = primType p
-tc (App f args) = checkFunCall (tc f) (fmap tc args)
+-- only programs that result in Bool or Num are runnable in this language
+typeChecks :: Exp -> Bool
+typeChecks e = elem (checkExp e) [BoolT, NumT]
+
+checkExp :: Exp -> Type
+checkExp (LitInt  i) = NumT
+checkExp (LitBool b) = BoolT
+checkExp (PrimExp p) = error $ "unapplied function: " ++ show p
+checkExp (App (PrimExp p) args) = checkFunCall (primType p) (fmap checkExp args)
 
 checkFunCall :: Type -> [Type] -> Type
-checkFunCall funT ([lt,rt]) | funT == polyFun = if lt == rt then BoolT else typeMismatch lt rt
-checkFunCall (ArrowT i o) []     = error "function not applied to any arguments"
+checkFunCall Poly ([lt,rt])      = if lt == rt then BoolT else typeMismatch lt rt
+checkFunCall Poly bad            = error $ "== applied to invalid arguments: " ++ show bad
 checkFunCall (ArrowT i o) (t:[]) = if i == t then o else typeMismatch i t
 checkFunCall (ArrowT i o) (t:ts) = if i == t then checkFunCall o ts else typeMismatch i t
-checkFunCall badT _ = error $ "attempt to apply arguments to: " ++ show badT
-
--- Num  -> Num  -> Num
-numFun  = ArrowT NumT  (ArrowT NumT  NumT)
--- a    -> a    -> a
-polyFun = ArrowT TyVar (ArrowT TyVar TyVar)
+checkFunCall (ArrowT i o) []     = error "function not applied to any arguments"
+checkFunCall BoolT _             = error $ "attempt to apply arguments to BoolT"
+checkFunCall NumT  _             = error $ "attempt to apply arguments to NumT"
 
 primType :: Prim -> Type
-primType Add     = numFun
-primType Sub     = numFun
-primType Mult    = numFun
-primType LTorEQ  = numFun
-primType Not     = ArrowT BoolT BoolT -- Bool -> Bool
-primType EqualTo = polyFun
+primType Not     = ArrowT BoolT BoolT
+primType EqualTo = Poly
+primType _       = ArrowT NumT (ArrowT NumT  NumT) 
 
 typeMismatch :: Type -> Type -> a
 typeMismatch t1 t2 = error $ "type mismatch: " ++ show t1 ++ ", " ++ show t2
